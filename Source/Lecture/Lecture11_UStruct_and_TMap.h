71fd1ecaee871ba8c2cc6cf65be357dc5f9e4bb0
HWNOTE <ekdrn1@naver.com>
2024-10-07 20:34:09 +0900

    FStudentData 별도의 헤더로 분리

--- Source/Lecture/Lecture11_UStruct_and_TMap.h
+++ Source/Lecture/Lecture11_UStruct_and_TMap.h
@@ -4,49 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+
+#include "StudentData.h"
+
 #include "Lecture11_UStruct_and_TMap.generated.h"
-
-USTRUCT()
-struct FStudentData
-{
-	GENERATED_BODY()
-
-	FStudentData()
-	{
-		Name = TEXT("홍길동");
-		ID = 0;
-	}
-
-	//구조체는 NewObject를 사용해서 만드는 것이 아니므로
-	//인자를 가진 생성자를 자유롭게 사용해도 된다.
-	FStudentData(FString InName, int32 InOrder)
-		: Name(InName)
-		, ID(InOrder)
-	{
-	}
-
-	//TSet, TMap의 키값으로 사용하기 위한 함수
-	//== operator
-	bool operator==(const FStudentData& InStudentData) const
-	{
-		return this->Name == InStudentData.Name;
-	}
-	//GetTypeHash 함수(언리얼 API인 GetTypeHash를 호출)
-	friend FORCEINLINE uint32 GetTypeHash(const FStudentData& InStudentData)
-	{
-		return GetTypeHash(InStudentData.Name);
-	}
-
-	//UPROPERTY 매크로를 넣을 때는 넣는 목적이 있어야 한다.
-	//1. 블루프린트에 표시한다던지
-	//2. 프로퍼티 시스템에 등록한다던지
-	//	-> 특히 UObject 포인터의 경우에는 프로퍼티 시스템에 반드시 등록해줘야 함
-	UPROPERTY()
-	FName Name;	//해시 키값으로도 사용될 예정이므로 FString이 아닌 FName 형태로 저장한다.
-
-	UPROPERTY()
-	int32 ID;
-};
 
 /**
  * 
---
+++ Source/Lecture/Lecture13_Serialization.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "Lecture13_Serialization.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LECTURE_API ULecture13_Serialization : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
